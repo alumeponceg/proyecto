c1e1dc649ec728370d71b204d0707470de2701eb
eponceg <alum.eponceg@iesalixar.org>
2020-04-27 03:13:32 +0200

    Update día 27 de abril, prototipo y mockUp

---
+++ Docs/Planificación_inicial/Prototipo/Rol_Paciente/prototype.md
@@ -0,0 +1,10 @@
+
+# PROTOTYPE
+
+Puede acceder al prototipo del proyecto en el siguiente enlace (*Este prototipo está asociado a aquellos usuarios que accedan a la web como pacientes*):
+
+## IMPORTANTE
+
+Por si las fuentes que se visualizan en el prototipo no se relacionan con las del mockUp, en esta misma carpeta adjunto las fuentes tipográficas descargadas, basta con incluirlas dentro de la carpeta fonts de la carpeta de archivos de axure.
+
+**[ver prototipo](https://wg2s58.axshare.com/)**
---
+++ Docs/Planificación_inicial/mockUp/Rol_Paciente/Javascript/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $("#form1").css({"height":$(window).height()+ "px"});
+    $("#form2").css({"height":$(window).height()+ "px"});
+    $(".indexbody").css({"height":$(window).height()+ "px"});
+    $(".body").css({"height":$(window).height()+ "px"});
+    $(".routine").css({"height":$(window).height()+ "px"});
+    $(".measurements").css({"height":$(window).height()+ "px"});
+    $(".appointment").css({"height":$(window).height()+ "px"});
+    $(".proximalDate").css({"height":$(".calendarjs").height()+ "px"});
+    $(".addDate").css({"height":$(".calendarjs").height()+ "px"});
+
+});
