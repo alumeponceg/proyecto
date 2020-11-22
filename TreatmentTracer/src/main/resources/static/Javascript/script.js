f4a9b7aa832393932e889da2cd92cbfea1f4d3c1
alumeponceg <alum.eponceg@iesalixar.org>
2020-11-22 23:45:31 +0100

    minor fix, fav icon

--- TreatmentTracer/src/main/resources/static/Javascript/script.js
+++ TreatmentTracer/src/main/resources/static/Javascript/script.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
     $("#form1").css({"height":$(window).height()+ "px"});
     $("#form2").css({"height":$(window).height()+ "px"});
     $(".indexbody").css({"height":$(window).height()+ "px"});
-    $(".body").css({"height":$(window).height()+ "px"});
-    $(".routine").css({"height":$(window).height()+ "px"});
+    
+    if($(".body").height()<= $(window).height()){
+    	 $(".body").css({"height":$(window).height()+ "px"});
+    }
+    if($(".routine").height()<= $(window).height()){
+   	 $(".routine").css({"height":$(window).height()+ "px"});
+   }
+//    $(".routine").css({"height":$(window).height()+ "px"});
     $(".measurements").css({"height":$(window).height()+ "px"});
     $(".appointment").css({"height":$(window).height()+ "px"});
     $(".proximalDate").css({"height":$(".calendarjs").height()+ "px"});
