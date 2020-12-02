93f0851b2451547af2a4f65fa8edc155377061d1
alumeponceg <alum.eponceg@iesalixar.org>
2020-12-02 15:46:17 +0100

    i18n finish

--- TreatmentTracer/src/main/java/org/iesalixar/eponceg/model/User.java
+++ TreatmentTracer/src/main/java/org/iesalixar/eponceg/model/User.java
@@ -18,8 +18,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Pattern;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
